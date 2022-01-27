07ae16f7dd11872c8f89872b7d6a878218eba65e
Felix Freidman <felixfreidman@gmail.com>
2022-01-27 15:18:19 +0800

    3:18PM Commit Did Basic Layout

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$("#userPhone").inputmask(); // // header-swiper
+// var swiper = new Swiper('#main-swiper', {
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next--header',
+//     prevEl: '.swiper-button-prev--header',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//     clickable: true,
+//   },
+//   loop: true,
+//   // autoplay: {
+//   //   delay: 2300,
+//   // },
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   speed: 800,
+//   watchSlidesProgress: true,
+//   watchVisibility: true,
+//   disableOnInteraction: true,
+// });
